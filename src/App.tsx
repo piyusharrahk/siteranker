55c63c14c2470696b02f6c1d42521c01405c2400
Piyush Arora <piyus@nebulainsights.io>
2025-09-10 15:46:01 +0530

    Wire routes: / -> HomePage, /brand/:slug -> BrandPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,21 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import ProtectedRoute from './components/ProtectedRoute';
-import Home from './pages/Home';
-import BrandPage from './pages/BrandPage';
-import BlogPage from './pages/BlogPage';
-import BlogPost from './pages/BlogPost';
-import ContactPage from './pages/ContactPage';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
-import PrivacyPage from './pages/PrivacyPage';
-import TermsPage from './pages/TermsPage';
-import Footer from './components/Footer';
+// src/App.tsx
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BrandPage from "./pages/BrandPage";
 
-function App() {
-  const location = useLocation();
-  
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
-
+export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Header />
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/brand/:brandName" element={<BrandPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/privacy" element={<PrivacyPage />} />
-        <Route path="/terms" element={<TermsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route 
-          path="/admin" 
-          element={
-            <ProtectedRoute requireAdmin>
-              <AdminPage />
-            </ProtectedRoute>
-          } 
-        />
+        {/* Home */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Brand detail */}
+        <Route path="/brand/:slug" element={<BrandPage />} />
+
+        {/* Fallback: send everything else to home (SPA routing) */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
